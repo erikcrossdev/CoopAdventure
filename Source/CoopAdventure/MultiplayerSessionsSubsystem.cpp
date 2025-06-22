ef9e52d22421316f08cc5a9f2a58532928d44c18
ErikCruz <erikcruzdev@gmail.com>
2025-06-22 16:25:01 -0300

    Basic session interface

---
+++ Source/CoopAdventure/MultiplayerSessionsSubsystem.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MultiplayerSessionsSubsystem.h"
+#include "OnlineSubsystem.h"
+
+UMultiplayerSessionsSubsystem::UMultiplayerSessionsSubsystem()
+{
+	//PrintString("MMS Constructor");
+}
+
+void UMultiplayerSessionsSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	//PrintString("MMS Initialize");
+	IOnlineSubsystem * OnlineSubsystem = IOnlineSubsystem::Get(); //return the online subsystem
+	if (OnlineSubsystem) 
+	{
+		FString SubSystemName = OnlineSubsystem->GetSubsystemName().ToString(); //the value set in the ini file, steam in our case
+		PrintString(SubSystemName);
+
+		SessionInterface = OnlineSubsystem->GetSessionInterface(); //pointer to the session interface
+		if (SessionInterface.IsValid()) {
+			PrintString("Session Interface is valid");
+		}
+	}
+}
+
+void UMultiplayerSessionsSubsystem::Deinitialize()
+{
+	//UE_LOG(LogTemp, Warning, TEXT("MMS Deinitialize"));
+}
+
+void UMultiplayerSessionsSubsystem::PrintString(const FString& message)
+{
+	if (GEngine) {
+		GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Cyan, message);
+	}
+}
---
+++ Source/CoopAdventure/MultiplayerSessionsSubsystem.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "Interfaces/OnlineSessionInterface.h"
+#include "MultiplayerSessionsSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COOPADVENTURE_API UMultiplayerSessionsSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	UMultiplayerSessionsSubsystem();
+
+	void Initialize(FSubsystemCollectionBase& Collection) override;
+	void Deinitialize() override;
+	void PrintString(const FString&);
+
+	IOnlineSessionPtr SessionInterface;
+};
