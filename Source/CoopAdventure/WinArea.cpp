5565da937454ee067f773d87731f897f6b236902
ErikCruz <erikcruzdev@gmail.com>
2025-07-05 14:04:37 -0300

    Create Win trigger

---
+++ Source/CoopAdventure/WinArea.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WinArea.h"
+#include "CoopAdventureCharacter.h"
+
+// Sets default values
+AWinArea::AWinArea()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	WinAreaBox = CreateDefaultSubobject<UBoxComponent>(TEXT("WinAreaBox"));
+	SetRootComponent(WinAreaBox);
+	WinCondition = false;
+}
+
+// Called when the game starts or when spawned
+void AWinArea::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AWinArea::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	if (HasAuthority() && !WinCondition) {
+		TArray<AActor*> OverlapActors;
+		WinAreaBox->GetOverlappingActors(OverlapActors, ACoopAdventureCharacter::StaticClass());
+		WinCondition = OverlapActors.Num() >= 2;
+		if (WinCondition) {
+			UE_LOG(LogTemp, Display, TEXT("We Won the game!!!"));
+		}
+	}
+}
+
